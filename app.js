484d50fbb1d44564a154d67e39c3d4689e6faef3
jml <student741@proton.me>
2024-12-11 19:58:09 +0100

    Finish project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const { body, validationResult } = require('express-validator')
 const path = require('node:path')
 const { Pool } = require('pg')
 const session = require('express-session')
@@ -18,6 +19,53 @@ const pool = new Pool({
 })
 pool.connect()
 
+//VALIDATORS
+const loginValidator = [
+  body('username').isLength({ min: 3 }).trim().escape(),
+  body('password').isLength({ min: 4 }).trim().escape(),
+]
+
+const registerValidator = [
+  body('username')
+    .isLength({ min: 3 })
+    .trim()
+    .escape()
+    .withMessage('Username must be at least 3 characters long'),
+  body('password')
+    .isLength({ min: 4 })
+    .withMessage('Password must be at least 4 characters long')
+    .trim()
+    .escape()
+    .withMessage('Password must be at least 4 characters long'),
+  body('confirmPassword')
+    .isLength({ min: 4 })
+    .trim()
+    .escape()
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match')
+      }
+      return true
+    })
+    .withMessage('Passwords do not match'),
+]
+
+const messageValidator = [
+  body('title')
+    .isLength({ min: 3 })
+    .trim()
+    .escape()
+    .withMessage('Title must be at least 3 characters long'),
+  body('content')
+    .isLength({ min: 3 })
+    .trim()
+    .escape()
+    .withMessage('Content must be at least 3 characters long'),
+]
+
+const codeValidator = [body('code').trim().escape()]
+
+//APP
 const app = express()
 
 //CONFIG
@@ -75,11 +123,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/register', (req, res) => {
-  res.render('register')
+  res.render('register', { errors: [] })
 })
 
-app.get('/home', (req, res) => {
-  res.render('home')
+app.get('/home', async (req, res) => {
+  try {
+    const messages = await pool.query('SELECT * FROM messages;')
+    res.render('home', { errors: [], messages: messages.rows })
+  } catch (error) {
+    res.render('home', { errors: [error.message], messages: [] })
+  }
+})
+
+app.get('/message', (req, res) => {
+  res.render('message', { errors: [] })
+})
+
+app.get('/club', (req, res) => {
+  res.render('club', { errors: [] })
+})
+
+app.get('/admin', (req, res) => {
+  res.render('admin', { errors: [] })
 })
 
 app.get('/logout', (req, res, next) => {
@@ -93,13 +158,27 @@ app.get('/logout', (req, res, next) => {
 
 app.post(
   '/',
+  loginValidator,
+  (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.render('login', { errors: errors.array() })
+    }
+    next()
+  },
   passport.authenticate('local', {
     successRedirect: '/home',
     failureRedirect: '/',
   })
 )
 
-app.post('/register', async (req, res, next) => {
+app.post('/register', registerValidator, async (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.render('register', {
+      errors: errors.array(),
+    })
+  }
   try {
     bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
       if (err) {
@@ -109,11 +188,96 @@ app.post('/register', async (req, res, next) => {
           'INSERT INTO users (username, password) VALUES ($1, $2)',
           [req.body.username, hashedPassword]
         )
-        res.redirect('/home')
+        res.redirect('/')
       }
     })
   } catch (error) {
     return next(error)
+  }
+})
+
+app.post('/message', messageValidator, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.render('message', {
+      errors: errors.array(),
+    })
+  }
+  const title = req.body.title
+  const message = req.body.content
+  let author = ''
+  try {
+    const result = await pool.query(
+      'SELECT (username) FROM users WHERE id = $1',
+      [req.user.id]
+    )
+    author = result.rows[0].username
+  } catch (error) {
+    res.render('message', { errors: [error] })
+  }
+  try {
+    const userId = req.user.id
+    await pool.query(
+      'INSERT INTO messages (user_id, title, message, author) VALUES ($1, $2, $3, $4)',
+      [userId, title, message, author]
+    )
+    res.redirect('/home')
+  } catch (error) {
+    res.render('message', { errors: [error] })
+  }
+})
+
+app.post('/club', codeValidator, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.render('club', {
+      errors: errors.array(),
+    })
+  }
+  const code = req.body.code
+  if (code === process.env.CLUBCODE) {
+    try {
+      await pool.query('UPDATE users SET member = true WHERE id = $1', [
+        req.user.id,
+      ])
+      res.redirect('/home')
+    } catch (error) {
+      res.render('club', { errors: [error.message] })
+    }
+  } else {
+    res.render('club', { errors: ['Invalid code'] })
+  }
+})
+
+app.post('/admin', codeValidator, async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.render('admin', {
+      errors: errors.array(),
+    })
+  }
+  const code = req.body.code
+  if (code === process.env.ADMINCODE) {
+    try {
+      await pool.query('UPDATE users SET admin = true WHERE id = $1', [
+        req.user.id,
+      ])
+      res.redirect('/home')
+    } catch (error) {
+      res.render('admin', { errors: [error.message] })
+    }
+  } else {
+    res.render('admin', { errors: ['Invalid code'] })
+  }
+})
+
+app.post('/delete', async (req, res) => {
+  const id = req.body.id
+  try {
+    await pool.query('DELETE FROM messages WHERE id = $1', [id])
+    res.redirect('/home')
+  } catch (error) {
+    res.render('home', { errors: [error.message] })
   }
 })
 
